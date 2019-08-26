a3c10bc0af943059b00a902dad61439571e7a8ce
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-08-26 11:11:02 +0100

    Lesson on objects with functions

--- object.js
+++ object.js
@@ -1,22 +1,30 @@
-// objects with functions
+/* In an object, the retutn keyword can also be use to return a object which can be called in the function*/
 
 let myBook = {
     title: 'Animal Farm',
     author: 'Mere Daniel',
-    pageCount: 326
+    pageCount: 326,
+    published: 1998
 }
 
 let otherBook = {
     title: 'Nigeria is in trouble',
     author: 'Mere Ujunwa',
-    pageCount: 452
+    pageCount: 452,
+    published: 1998
 }
 let getSummary = function(book){
-    console.log(`The book ${book.title} was written by ${book.author} and they are ${book.pageCount} pages`)
+    return {
+        summary: `The book ${book.title} was written by ${book.author} and they are ${book.pageCount} pages`,
+        otherSummary: `The important thing about the two books is that they were both published in ${book.published}, that is the same year`
+    }
 }
-getSummary(myBook)
-getSummary(otherBook)
+let obtainedSummary = getSummary(myBook)
+let otherObtainedSummary = getSummary(otherBook)
+console.log(obtainedSummary.summary)
+console.log(otherObtainedSummary.otherSummary)
 
-//Outputs
-The book Animal Farm was written by Mere Daniel and they are 326
-The book Nigeria is in trouble was written by Mere Ujunwa and they are 452
+// Outputs
+The book Animal Farm was written by Mere Daniel and they are 326 pages
+The important thing about the two books is that they were both published in 1998, that is the same year
+
