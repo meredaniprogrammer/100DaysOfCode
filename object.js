68f88385e933c769b9d20464be5246c4fed4f291
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-08-29 10:52:49 +0100

    Lesson 4 on objects

--- object.js
+++ object.js
@@ -1,30 +1,17 @@
-/* In an object, the retutn keyword can also be use to return a object which can be called in the function*/
+// Objects with methods.....A method is an object property whose value is a function
 
-let myBook = {
-    title: 'Animal Farm',
-    author: 'Mere Daniel',
-    pageCount: 326,
-    published: 1998
-}
+let restaurant = {
+    name: 'NSB',
+    guestCapacity: 75,
+    guestCount: 0,
+    checkAvailability: function(partySize) {
+        let seatSize = this.guestCapacity - this.guestCount
+        return seatSize >= partySize
 
-let otherBook = {
-    title: 'Nigeria is in trouble',
-    author: 'Mere Ujunwa',
-    pageCount: 452,
-    published: 1998
-}
-let getSummary = function(book){
-    return {
-        summary: `The book ${book.title} was written by ${book.author} and they are ${book.pageCount} pages`,
-        otherSummary: `The important thing about the two books is that they were both published in ${book.published}, that is the same year`
     }
 }
-let obtainedSummary = getSummary(myBook)
-let otherObtainedSummary = getSummary(otherBook)
-console.log(obtainedSummary.summary)
-console.log(otherObtainedSummary.otherSummary)
+let status = restaurant.checkAvailability(4)
+console.log(status)
 
-// Outputs
-The book Animal Farm was written by Mere Daniel and they are 326 pages
-The important thing about the two books is that they were both published in 1998, that is the same year
+// Returns True
 
