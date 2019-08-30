d80780ebe29c508f59547779bc30b790bac37d19
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-08-30 12:20:29 +0100

    A const object variable

---
+++ const.js
@@ -0,0 +1,10 @@
+//When const is used to declare a variable, the valu can't re re-assigned. But if it's an object like the example we have below, the value can always be changed
+
+const person = {
+    name: 'Mere Daniel',
+    age: 20
+}
+
+person.name = 'Mere Ugochukwu'
+
+console.log(person)
